ff2da15833bb05934bd25999de2614e5c48fee4b
Naea Fairbrother <p4217876@live.tees.ac.uk>
2019-03-15 23:29:05 +0000

    Main Activity Drawer

--- app/src/main/java/com/teesside/yellowann/MainActivity.java
+++ app/src/main/java/com/teesside/yellowann/MainActivity.java
@@ -1,16 +1,40 @@
 package com.teesside.yellowann;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 public class MainActivity extends AppCompatActivity
 {
+    private DrawerLayout Drawer;
+    private NavigationView NavView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        Drawer = findViewById(R.id.drawer_layout);
+        NavView = findViewById(R.id.navigation_view);
+
+        NavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                menuItem.setChecked(true);
+                Drawer.closeDrawers();
+
+                return true;
+            }
+        });
     }
 }
+
+
+
